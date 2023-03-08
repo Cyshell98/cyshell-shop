a7cf794c5bfffc9260f88437eb11b8c0aad5b364
Cyshell98 <chen19981011@outlook.com>
2023-03-08 21:23:36 +0800

    goodslist开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		Goods_list
+		<view class="goods-list">
+			<view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+				<block>
+					<my-goods :goods="item"></my-goods>
+				</block>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +14,110 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					// 查询关键词
+					query: '',
+					// 商品分类Id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示多少条数据
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				// 默认的空图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			console.log(this.queryObj);
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(cb) {
+				// 打开节流阀
+				this.isloading = true
+				// 发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				// 关闭节流阀
+				this.isloading = false
+
+				// 只要数据请求完毕，就立刻按需调用cb回调函数
+				cb && cb()
+
+				if (res.meta.status !== 200) return uni.$showMsg()
+
+				// 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			}
+		},
+		// 触底的事件
+		onReachBottom() {
+			// 判断是否还有下一页数据
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+			// 判断是否正在请求其他数据，如果有则不发起额外的请求
+			if (this.isloading) return
+
+			// 让页码值 +1
+			this.queryObj.pagenum += 1
+			// 重新获取列表数据
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			// 重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+
+			// 重新发起请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
+		},
+		// 点击跳转到商品详情页面
+		gotoDetail(item) {
+			uni.navigateTo({
+				url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+			})
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+
+		.goods-item-left {
+			margin-right: 5px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-price {
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
 </style>
